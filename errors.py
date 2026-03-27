00e2dfb40e64409bbd86caefef54dd0e487fa2be
guff192 <kochkons1@ya.ru>
2026-03-27 17:17:32 +0400

    :sparkles: Added new error type for non-positive rectangle area

--- errors.py
+++ errors.py
@@ -4,3 +4,8 @@ import sys
 def exit_with_err_description(description: str) -> None:
     print(description, file=sys.stderr)
     exit(1)
+
+
+class NonPositiveRectangleAreaException(Exception):
+    def __init__(self) -> None:
+        super().__init__("The area of this rectangle is not a positive number!")
