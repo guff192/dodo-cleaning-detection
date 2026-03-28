29633005678bac582a7bb43d500dfd4cb49a1ad2
guff192 <kochkons1@ya.ru>
2026-03-28 18:02:20 +0400

    :sparkles: Added use of tracker and geometry logic to video processing funciton

--- video.py
+++ video.py
@@ -4,6 +4,8 @@ import cv2
 
 from detector import get_people_boxes
 from errors import exit_with_err_description
+from geometry import get_bottom_center_point, is_table_occupied_by_person
+from tracker import TableEventType, TableTracker
 
 if TYPE_CHECKING:
     from cv2.typing import Rect
@@ -36,32 +38,48 @@ def run_preview(video_path: str, roi: Rect) -> None:
     print("Starting video preview, press 'q' to quit...")
 
     window_name = "Video detection preview"
+    tracker = TableTracker()
     frame_count = 0
     cached_boxes = []
+    events = []
     while True:
         ret, frame = cap.read()
         if not ret:
             break
 
-        cv2.rectangle(frame, (x, y), (x + w, y + h), table_color, 2)
-
-        text = "Table zone"
-        cv2.putText(frame, text, (x, y - 10), cv2.FONT_HERSHEY_PLAIN, 1, table_color, 2)
-
         if frame_count % 15 == 0:
             cached_boxes = get_people_boxes(frame)
+
+        current_time = cap.get(cv2.CAP_PROP_POS_MSEC) / 1000
+        is_anyone_in_zone = False
 
         for box in cached_boxes:
             x1, y1, x2, y2 = box
 
+            if is_table_occupied_by_person(roi, box):
+                is_anyone_in_zone = True
+                table_color = (0, 0, 255)
+
             cv2.rectangle(frame, (x1, y1), (x2, y2), person_color, 2)
 
             # Bottom-Center Point (BCP) coordinates
-            bcp_x = int((x1 + x2) / 2)
-            bcp_y = y2
+            bcp_xy = get_bottom_center_point((x1, y1, x2, y2))
 
             # Draw BCP, -1 makes it filled
-            cv2.circle(frame, (bcp_x, bcp_y), 5, point_color, -1)
+            cv2.circle(frame, bcp_xy, 5, point_color, -1)
+
+        event = tracker.update(is_anyone_in_zone, current_time)
+        if event is not None:
+            events.append(event)
+            print(f"Table is {event.type.lower()} at {event.timestamp}")
+            if event.type == TableEventType.FREED:
+                table_color = (0, 255, 0)
+
+        cv2.rectangle(frame, (x, y), (x + w, y + h), table_color, 2)
+        text = "Table zone"
+        cv2.putText(frame, text, (x, y - 10), cv2.FONT_HERSHEY_PLAIN, 1, table_color, 2)
+
+        frame_count += 1
 
         cv2.imshow(window_name, frame)
 
